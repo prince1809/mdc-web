38517bae7f745189bed3d44505e5e31711eae84d
prince <prince18pat@gmail.com>
2019-07-06 01:52:21 +0900

    add button component

--- stories/html/button.stories.js
+++ stories/html/button.stories.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
+import '../styles/button.scss'
 
-import { Button, Welcome } from '@storybook/react/demo';
+class DefaultButton extends React.Component {
+
+  renderButtonVariant(title, variantClass) {
+    return (
+      <div>
+        <h3>{title}</h3>
+        <button className={`demo-button mdc-button ${variantClass}`} ref={this.initRipple}>
+          <span>Default</span>
+        </button>
+        <button>
+          <span>Dense</span>
+        </button>
+        <button>
+          <i>favorite</i>
+          <span>Icon</span>
+        </button>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderButtonVariant('Text Button')}
+        {this.renderButtonVariant('Raised Button')}
+        {this.renderButtonVariant('Unelevated Button')}
+        {this.renderButtonVariant('Outlined Button')}
+        {this.renderButtonVariant('Shaped Button')}
+      </div>
+    )
+  }
+}
 
 
 storiesOf('Button', module)
-  .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+  .add('to Storybook', () => <DefaultButton />);
