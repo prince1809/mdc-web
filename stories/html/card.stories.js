408de1927cd57a309995c6e529990d1eb6e0e5ed
prince <prince18pat@gmail.com>
2019-07-08 01:43:56 +0900

    add card demos

---
+++ stories/html/card.stories.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import classnames from 'classnames';
+
+import { storiesOf } from '@storybook/react';
+
+import '../styles/card'
+
+const CardTypes = {
+  basic: 'basic',
+  basicHeader: 'basicHeader',
+  basicMediaText: 'basicMediaText',
+  basicButtons: 'basicButtons',
+  basicIcons: 'basicIcons',
+  uiControl: 'uiControl',
+};
+
+const classes = ({ className, shaped, type }) => {
+  const { basicHeader, basicMediaText, uiControl } = CardTypes;
+  return classnames('mdc-card demo-card',
+    {
+      'demo-card-shaped': shaped,
+      'demo-basic-with-header': type === basicHeader,
+      'demo-basic-with-text-over-media': type === basicMediaText,
+      'demo-ui-control': type === uiControl,
+    }, className);
+};
+
+class Card extends React.Component {
+  render() {
+    const { actions, image, type } = this.props;
+    const { basicHeader, basicMediaText, basicButtons, basicIcons, uiControl } = CardTypes;
+    return (
+      <div className={classes(this.props)}>
+        {type === basicHeader ? <CardHeader /> : null}
+        <div>
+          {image ? <CardMedia showText={type === basicMediaText} square={type === uiControl} /> : null}
+          {type !== basicHeader && type !== basicMediaText ? <CardHeader /> : null}
+
+          {type !== uiControl ? <CardSecondary /> : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+const CardHeader = () => {
+  return (
+    <div className='demo-card__primary'>
+      <h2>Our Changing Planet</h2>
+      <h3>by Kurt Wagner</h3>
+    </div>
+  );
+}
+
+const CardSecondary = () => {
+  return (
+    <div className="demo-card__secondary">
+      Visit ten places on our planet that are undergoing the biggest changes today.
+    </div>
+  )
+}
+
+const CardMedia = ({ square = false, showText = false }) => {
+  return (
+    <div>
+      {showText ? <div>
+        <CardHeader />
+      </div> : null}
+    </div>
+  )
+}
+
+const CardDemos = () => {
+  return (
+    <section className='demo-card-collection'>
+      <Card image />
+      <Card actions />
+      <Card actions shaped />
+    </section>
+  );
+}
+
+storiesOf('Card', module)
+  .add('Demos', () => <CardDemos />);
